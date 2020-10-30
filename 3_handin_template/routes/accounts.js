f24c5b9dde86f9e288e414518b9da5775ccff619
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-10-30 18:20:23 +0100

    Print Balance Print all accounts

--- 3_handin_template/routes/accounts.js
+++ 3_handin_template/routes/accounts.js
@@ -1,28 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Account = require('../models/account');
 
-//Endpoint for all users
-router.get('/', async (req, res) => {
-    try {
-        // 1. return accounts from database instead
-        res.end("This is the GET endpoint on accounts")
-    } catch (err) {
-        console.log({message: err})
-    };
+const accountModel = require("../models/account.js");
+/*
+//Endpoint for all accounts
+router.get("/", async (req, res) => {
+  try {
+    res.end("This is the GET endpoint on accounts");
+  } catch (err) {
+    console.log({ message: err });
+  }
 });
-
+*/
 //Endpoint for adding user
-router.post('/', async (req, res) => {
-    const user = new Account({
-        //enter correct fields
-    });
+router.post("/", async (req, res) => {
+  const account = {
+    name: req.body.name,
+    balance: req.body.balance,
+  };
+  const message =
+    "This is the post endpoint for accounts. We received the data seen under account";
 
-    res.json(user);
+  res.json({
+    message,
+    account,
+  });
 });
 
-// Implement a new endpoint, that will be able to return a specific account by id. 
-// instead of just printing, return the actual account. 
+// Implement a new endpoint, that will be able to return a specific balance by name.
+router.get("/:name", async function (req, res) {
+  let oneAccount = await accountModel
+    .findOne({ firstName: req.params.name })
+    .exec();
+  let oneBalance = oneAccount.balance;
+  res.send(req.params.name + "s balance is: " + oneBalance);
+  //res.send(oneAccount.balance);
+});
 
+// Implement endpoint for showing all accounts
+router.get("/", async (req, res) => {
+  accountModel.find().then((accounts) => res.json({ accounts }));
+});
 
 module.exports = router;
