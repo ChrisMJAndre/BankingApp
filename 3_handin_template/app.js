3fdc9a583a8393749ebecb22ea6b58761345eb0e
ChrisMJAndre <chan19af@student.cbs.dk>
2020-11-02 20:12:26 +0100

    transfer code

--- 3_handin_template/app.js
+++ 3_handin_template/app.js
@@ -3,6 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const db = require("./db.js");
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
 
 //Added Json Body-parser
 app.use(bodyParser.json());
@@ -24,4 +35,8 @@ app.listen(8080, () => {
     console.log("Database connected");
   });
   console.log("Server listening on 8080");
+});
+
+sslServer.listen(3443, () => {
+  console.log("server is listening on 3443");
 });
