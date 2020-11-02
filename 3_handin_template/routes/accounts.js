27c1bf52e21ace5ac165f0f1427902076d01d25e
ChrisMJAndre <chan19af@student.cbs.dk>
2020-11-02 19:35:05 +0100

    Remove useless endpoints

--- 3_handin_template/routes/accounts.js
+++ 3_handin_template/routes/accounts.js
@@ -2,30 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const accountModel = require("../models/account.js");
-/*
-//Endpoint for all accounts
-router.get("/", async (req, res) => {
-  try {
-    res.end("This is the GET endpoint on accounts");
-  } catch (err) {
-    console.log({ message: err });
-  }
-});
-*/
-//Endpoint for adding user
-router.post("/", async (req, res) => {
-  const account = {
-    name: req.body.name,
-    balance: req.body.balance,
-  };
-  const message =
-    "This is the post endpoint for accounts. We received the data seen under account";
-
-  res.json({
-    message,
-    account,
-  });
-});
 
 // Implement a new endpoint, that will be able to return a specific balance by name.
 router.get("/:name", async function (req, res) {
