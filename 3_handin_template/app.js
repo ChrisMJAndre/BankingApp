50c4a7bad59e5b0436d28da734a4f8134c78bc18
ChrisMJAndre <chan19af@student.cbs.dk>
2020-11-02 20:18:46 +0100

    Implemented SSL for banking App

--- 3_handin_template/app.js
+++ 3_handin_template/app.js
@@ -7,21 +7,23 @@ const path = require("path");
 const fs = require("fs");
 const https = require("https");
 
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
-  },
-  app
-);
-
 //Added Json Body-parser
 app.use(bodyParser.json());
 
 //Import Routes
 const accountRoute = require("./routes/accounts");
+const router = require("./routes/accounts");
 
 app.use("/accounts", accountRoute);
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app,
+  router
+);
 
 //Initial route
 app.get("/", (req, res) => {
@@ -30,13 +32,9 @@ app.get("/", (req, res) => {
 
 //Start listening
 
-app.listen(8080, () => {
+sslServer.listen(3443, () => {
   db.getConnection().then(() => {
     console.log("Database connected");
   });
-  console.log("Server listening on 8080");
-});
-
-sslServer.listen(3443, () => {
   console.log("server is listening on 3443");
 });
